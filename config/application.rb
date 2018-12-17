90b02bf13a9bd62de1ed334009d31712410e43e6
longhaoran <longhr@wfl-ischool.cn>
2018-12-17 09:01:00 +0800

    时区设置

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,7 @@ module UltimateDemo
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.time_zone = 'Asia/Shanghai'
 
 
   end
