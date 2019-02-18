f6b26a3b0547603ef7f30f0b8802177e522888c0
longhaoran <longhr@wfl-ischool.cn>
2019-02-18 10:40:11 +0800

    更新脚本

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 git pull
 bundle install
-RAILS_ENV=production rails db:migrate
-RAILS_ENV=production rails assets:precompile
-RAILS_ENV=production rails s -b 0.0.0.0
+RAILS_ENV=production rake db:migrate
+RAILS_ENV=production rake assets:precompile
+RAILS_ENV=production rails s -b 0.0.0.0 -d
