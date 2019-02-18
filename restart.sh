14d7a5ea66dd5e82c6c938f7054ee38058731626
longhaoran <longhr@wfl-ischool.cn>
2019-02-18 10:42:27 +0800

    修改脚本，增加杀进程

--- restart.sh
+++ restart.sh
@@ -1,5 +1,6 @@
 git pull
 bundle install
+kill `cat /home/longhr/projects/UltimateDemo/tmp/pids/server.pid`
 RAILS_ENV=production rake db:migrate
 RAILS_ENV=production rake assets:precompile
 RAILS_ENV=production rails s -b 0.0.0.0 -d
