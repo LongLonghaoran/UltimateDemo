25b67c5ca78c3585e5c4f7116c378f38aeea6f05
longhaoran <longhr@wfl-ischool.cn>
2019-02-21 15:40:13 +0800

    添加puma配置
    production使用unix sockes连接

--- config/puma.rb
+++ config/puma.rb
@@ -1,28 +1,26 @@
+if ENV['RAILS_ENV'] == 'production'
 # Change to match your CPU core count
-workers 2
+  workers 1
 # Min and Max threads per worker
-threads 1, 6
-​
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-​
+  threads 1, 6
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
 # Default to production
-rails_env = ENV['RAILS_ENV'] || "development"
-environment rails_env
-​
+  environment 'production'
 # Set up socket location
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-​
+  bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+
 # Logging
-stdout_redirect "#{app_dir}/tmp/log/puma.stdout.log", "#{app_dir}/tmp/log/puma.stderr.log", true
-​
+# stdout_redirect "#{app_dir}/tmp/log/puma.stdout.log", "#{app_dir}/tmp/log/puma.stderr.log", true
+
 # Set master PID and state locations
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-activate_control_app
-​
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  pidfile "#{app_dir}/tmp/pids/puma.pid"
+  state_path "#{app_dir}/tmp/pids/puma.state"
+  activate_control_app
+
+  on_worker_boot do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
 end
